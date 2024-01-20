b33ed9997e73b0ea69f806680900c61138013888
Diamond <lolypop0192@gmail.com>
2024-01-21 01:58:18 +0200

    Adding Club & Home Index.Html Views

--- RunGroopWebApp/obj/Debug/net6.0/RunGroopWebApp.AssemblyInfo.cs
+++ RunGroopWebApp/obj/Debug/net6.0/RunGroopWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunGroopWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b95999f0fb4d35d793a7d2495524d36ea45857f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb5859bca43e670567f55e91738ab8145ded08a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RunGroopWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RunGroopWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
